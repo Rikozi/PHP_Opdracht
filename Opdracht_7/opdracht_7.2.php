6fab473034fdda0b4816ff5a62fefe04a032a6e5
Rikozi <120098292+Rikozi@users.noreply.github.com>
2022-12-09 13:48:19 +0100

    Update opdracht_7.2.php

--- Opdracht_7/opdracht_7.2.php
+++ Opdracht_7/opdracht_7.2.php
@@ -7,6 +7,40 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="#" method="post">
+        <p>Getal 1 <input type="number" name="getal1" value="getal1"></p> 
+        <input type="radio" name="rekenen" value="optellen"> Optellen
+        <input type="radio" name="rekenen" value="aftrekken"> Aftrekken
+        <input type="radio" name="rekenen" value="vermenigvuldigen"> Vermenigvuldigen
+        <input type="radio" name="rekenen" value="delen"> Delen
+        <p>Getal 2 <input type="number" name="getal2" value="getal2"></p> 
+        <input type="submit" name="Berekenen" value="berekenen">        
+    </form>
+
+    <?php
+        $waarde = $_POST["rekenen"];
+        $getal1 = $_POST["getal1"];
+        $getal2 = $_POST["getal2"];
+
+        switch($waarde) {
+            case"optellen":
+                $som = $getal1 + $getal2;
+                echo"$getal1 + $getal2 = $som";
+            break;
+            case"aftrekken":
+                $som = $getal1 - $getal2;
+                echo"$getal1 - $getal2 = $som";
+            break;
+            case"vermenigvuldigen":
+                $som = $getal1 * $getal2;
+                echo"$getal1 * $getal2 = $som";
+            break;
+            case"delen":
+                $som = $getal1 / $getal2;
+                echo"$getal1 / $getal2 = $som";
+            break;
+        }
+
+    ?>
 </body>
 </html>
