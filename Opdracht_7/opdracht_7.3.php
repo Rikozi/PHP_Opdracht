efc5540b5c2edbb4e23ec23ed7eeede935f3f7da
Rikozi <120098292+Rikozi@users.noreply.github.com>
2022-12-09 13:48:39 +0100

    Update opdracht_7.3.php

--- Opdracht_7/opdracht_7.3.php
+++ Opdracht_7/opdracht_7.3.php
@@ -7,6 +7,33 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="#" method="post">
+    <input type="radio" name="kleur" value="rood">Rood
+    <input type="radio" name="kleur" value="groen">Groen
+    <input type="radio" name="kleur" value="blauw">Blauw
+    <input type="radio" name="kleur" value="roze">Roze
+    <input type="submit" name="instellen" value="instellen">
+    </form>
     
+    <?php
+    if(isset($_POST["kleur"])) {
+        $kleur = $_POST["kleur"];
+
+        switch($kleur) {
+            case"rood":
+                echo"<style>body{background-color: red;}</style>";
+            break;
+            case"groen":
+                echo"<style>body{background-color: ;}</style>";
+            break;
+            case"blauw":
+                echo"<style>body{background-color: blue;}</style>";
+            break;
+            case"roze":
+                echo"<style>body{background-color: pink;}</style>";
+            break;
+        }
+    }
+    ?>
 </body>
 </html>
